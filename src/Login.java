12cd20a77663cc689d22924d83906854a5be51b6
Rohan-Navale <rohannavalekannadiga@gmail.com>
2023-08-04 22:57:53 +0530

    2/12 Done. Completed at 10.50 pm 04/08/2024

--- src/Login.java
+++ src/Login.java
@@ -1,7 +1,13 @@
+//Sign In -> Application Form -> Additional Details -> Account Details -> ATM Interface Deposit
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login extends JFrame{
+public class Login extends JFrame implements ActionListener {
+    JButton login,clear,register; // since it should be accessed by the actionlistner so it is declared globally
+    JTextField cardTextField;
+    JPasswordField pinTextField; //since password should be hidden while typing JPasswordField should be used.
     Login(){
 
         setTitle("Automated Teller Machine");
@@ -14,9 +20,9 @@ public class Login extends JFrame{
 //        label.setBounds(70, 10, 100, 100);
 //        add(label);
 
-        JLabel text = new JLabel("CENTRAL BANK ATM Systems"); // JLanble is used to get text or image on frame
+        JLabel text = new JLabel("CENTRAL BANK ATM Systems"); // JLable is used to get text or image on frame
         text.setFont(new Font("Roboto", Font.BOLD,24));
-        text.setForeground(Color.WHITE);
+        text.setForeground(Color.WHITE);    //foregroundColor means text color only
         text.setBounds(219,38, 362, 70);
         add(text);
 
@@ -32,20 +38,65 @@ public class Login extends JFrame{
         cardno.setBounds(219,133, 156, 35);
         add(cardno);
 
+        cardTextField = new JTextField();
+        cardTextField.setBounds(362,136,192,30);
+        cardTextField.setFont(new Font("Roboto", Font.BOLD,18));
+        add(cardTextField);
+
         JLabel pin = new JLabel("PIN :");
         pin.setFont(new Font("Roboto", Font.BOLD,20));
         pin.setForeground(Color.WHITE);
-        pin.setBounds(219,183, 53, 35);
+        pin.setBounds(311,183, 53, 35);
         add(pin);
+
+        pinTextField = new JPasswordField();
+        pinTextField.setBounds(362,186,192,30);
+        pinTextField.setFont(new Font("Roboto", Font.BOLD,18));
+        add(pinTextField);
+
+        login = new JButton("Sign In");
+        login.setBounds(450, 238, 100, 28);
+        login.addActionListener(this);
+        login.setForeground(Color.white);
+        login.setBackground(Color.DARK_GRAY);
+        add(login);
+
+        clear = new JButton("Clear");
+        clear.setBounds(360, 238, 80, 28);
+        clear.setForeground(Color.white);
+        clear.addActionListener(this);
+        clear.setBackground(Color.DARK_GRAY);
+        add(clear);
+
+        register = new JButton("Register");
+        register.setBounds(360, 276, 190, 28);
+        register.setForeground(Color.white);
+        register.addActionListener(this);
+        register.setBackground(Color.DARK_GRAY);
+        add(register);
 
 
         setSize(800,400); // Set the Frame Size
-        getContentPane().setBackground(Color.getColor("clr", 333333));
+        getContentPane().setBackground(Color.gray);
         setVisible(true);
         setLocation(350,200);
 
 
 
+
+
+
+
+    }
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource() == clear){
+            cardTextField.setText("");
+            pinTextField.setText("");
+        }else if(ae.getSource() == register){
+
+        }else if(ae.getSource() == login){
+
+        }
     }
     public static void main(String[] args){
         new Login();
